c97d35ef9e5f621a5149346cc3494e95ef33e427
shameer5 <61453985+shameer5@users.noreply.github.com>
2021-06-27 04:08:18 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import userRoutes from './routes/user.js';
 const app = express();
 dotenv.config();
 
-// this is to set yp our body parser properly
+// this is to set up your body parser properly
 //bodyParser.json with 30mb is set for when we send images which are large in size
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -21,9 +21,9 @@ It will automatically populate the PORT varaible*/
 const PORT = process.env.PORT|| 5000;
 
 /* Connecting to the the DB
-    If connect then it will appl wiil listen */
+    If it connects then application will listen */
 /* @param - 1st -> mention the connection url to the DB
-        2nd -> just to avoid warnings in the console */
+   @param - 2nd -> just to avoid warnings in the console */
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
